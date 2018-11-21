9250073343c855df1e8c2188d0364ad444afeeec
Vladislav_K <v.kurashkevich@gmail.com>
2018-11-21 19:16:22 +0300

    first commit

---
+++ src/main/java/com/completeboot/course/ShopBootApplication.java
@@ -0,0 +1,12 @@
+package com.completeboot.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopBootApplication.class, args);
+	}
+}
