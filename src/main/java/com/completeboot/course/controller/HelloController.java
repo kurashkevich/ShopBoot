328fd716a4d77717f4fdc49ba41fec9bc28ad523
Vladislav_K <v.kurashkevich@gmail.com>
2018-11-23 08:47:23 +0300

    after creating first module

--- src/main/java/com/completeboot/course/controller/HelloController.java
+++ src/main/java/com/completeboot/course/controller/HelloController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
     private NotificationService notificationService;
 
     @Value("${helloController.msg}")
